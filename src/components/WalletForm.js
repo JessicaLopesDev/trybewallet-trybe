c5dd49f6d37c5021f29a27eab56f7d05e6e77364
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-03-10 07:06:21 -0300

    Criando formulario de adicionar despesas

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,45 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <form>
+        <input
+          type="number"
+          placeholder="Valor"
+          data-testid="value-input"
+        />
+
+        <input
+          type="text"
+          placeholder="Descrição"
+          data-testid="description-input"
+        />
+
+        <select data-testid="currency-input">
+          <option>Real</option>
+          <option>Dólar</option>
+          <option>Euro</option>
+        </select>
+
+        <select data-testid="method-input">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </select>
+
+        <select data-testid="tag-input">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+
+        <button
+          type="submit"
+        >
+          Adicionar despesa
+        </button>
+      </form>
     );
   }
 }
