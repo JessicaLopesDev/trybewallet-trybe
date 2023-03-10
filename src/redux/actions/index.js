261fe1f8aa7bff5dd8196d45eb1f0582edd85fde
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-03-10 07:35:07 -0300

    Cria reducer e action da wallet

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,12 @@
 export const USER_DATA = 'USER_DATA';
+export const WALLET_DATA = 'WALLET_DATA';
 
 export const addEmailAction = (userData) => ({
   type: USER_DATA,
   payload: userData,
+});
+
+export const addCurrenciesAction = (walletData) => ({
+  type: WALLET_DATA,
+  payload: walletData,
 });
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,22 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET_DATA } from '../actions';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+    editor: false,
+    idToEdit: 0,
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_DATA: return {
+    ...state,
+    ...action.payload,
+  };
+  default: return state;
+  }
+};
+
+export default wallet;
