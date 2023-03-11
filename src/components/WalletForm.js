591b28a667e90d6a4ccb3a6fc2cdd21ce8b95782
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-03-11 13:58:51 -0300

    Criando estado local de expenses do walletForm

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -5,29 +5,71 @@ import { connect } from 'react-redux';
 import { fetchCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
+  state = {
+    id: 0,
+    value: 0,
+    description: '',
+    currency: '',
+    method: '',
+    tag: '',
+  };
+
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(fetchCurrencies());
   }
 
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({ [name]: value });
+  };
+
+  // handleSubmit = () => {
+  //   const { id, value, description, currency, method, tag } = this.state;
+
+  //   this.setState(() => ({
+  //     ...prevState,
+  //     id,
+  //     value,
+  //     description,
+  //     currency,
+  //     method,
+  //     tag,
+  //   }));
+  // };
+
   render() {
+    const { id, value, description, currency, method, tag } = this.state;
     const { currenciesData } = this.props;
 
     return (
-      <form>
+      <form id={ id }>
         <input
           type="number"
           placeholder="Valor"
           data-testid="value-input"
+          name="value"
+          value={ value }
+          onChange={ this.handleChange }
         />
 
         <input
           type="text"
           placeholder="Descrição"
           data-testid="description-input"
+          name="description"
+          value={ description }
+          onChange={ this.handleChange }
         />
 
-        <select data-testid="currency-input">
+        <select
+          data-testid="currency-input"
+          name="currency"
+          value={ currency }
+          onChange={ this.handleChange }
+        >
           {
             // console.log(currenciesData)
             currenciesData.map((item, index) => (
@@ -41,13 +83,23 @@ class WalletForm extends Component {
           }
         </select>
 
-        <select data-testid="method-input">
+        <select
+          data-testid="method-input"
+          name="method"
+          value={ method }
+          onChange={ this.handleChange }
+        >
           <option>Dinheiro</option>
           <option>Cartão de crédito</option>
           <option>Cartão de débito</option>
         </select>
 
-        <select data-testid="tag-input">
+        <select
+          data-testid="tag-input"
+          name="tag"
+          value={ tag }
+          onChange={ this.handleChange }
+        >
           <option>Alimentação</option>
           <option>Lazer</option>
           <option>Trabalho</option>
